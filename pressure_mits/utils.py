0027e3da20a812d89d9a708b23b97310ba942b55
xaycxep <93213584+xaycxep@users.noreply.github.com>
2026-09-03 14:35:10 +0300

    Update utils.py

--- pressure_mits/utils.py
+++ pressure_mits/utils.py
@@ -1,1 +1,14 @@
-
+#utils.py
+def reformat_data(data: list) -> list:
+    res = []
+    for el in data:
+        res.append({
+                'act': el.get('act'),
+                'instrument_name': el.get('instrument_name'),
+                'serial_number': el.get('serial_number'),
+                'verification_date': el.get('verification_date'),
+                'next_verification_date':  el.get('next_verification_date'),
+                'note': el.get('note'),
+                'mit_link': el.get('mit_link')
+            })
+    return res
